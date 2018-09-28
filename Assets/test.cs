2e7c421757a50f3a7e535c76643fe651e75fcb96
dragon <43591718+koumori0115@users.noreply.github.com>
2018-09-28 11:15:42 +0900

    script

--- Assets/test.cs
+++ Assets/test.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour {
-    int i = 0;
 	// Use this for initialization
 	void Start () {
-        i = 3;
+        Debug.Log("222");
 	}
 	
 	// Update is called once per frame
