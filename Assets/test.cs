d0e91cf3968fec1bb6762943a4dc7280a908757d
dragon <43591718+koumori0115@users.noreply.github.com>
2018-09-28 11:08:21 +0900

    スクリプト変更

--- Assets/test.cs
+++ Assets/test.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour {
-
+    int i = 0;
 	// Use this for initialization
 	void Start () {
-		
+        i = 3;
 	}
 	
 	// Update is called once per frame
